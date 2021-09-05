7a3aff9acdbd9ec2a906c933de58d593d508db0d
Dulan <79652708+Dlnmdu@users.noreply.github.com>
2021-09-05 23:16:50 +0530

    Integrate google map

--- src/screens/Location/index.js
+++ src/screens/Location/index.js
@@ -1,18 +1,41 @@
 import React from 'react';
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, StyleSheet, SafeAreaView} from 'react-native';
 import style from './styles';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 const LocationScreen = () => {
   return (
-    <View>
-      <View style={style.imageView}>
-        <Image
+    <SafeAreaView style={styles.container}>
+      
+        
+          {/* <Image
           style={style.imageStyle}
           source={require('../../../assets/location.jpg')}
-        />
-      </View>
-    </View>
+        /> */}
+          <MapView
+            style={styles.map}
+            provider={PROVIDER_GOOGLE}
+            initialRegion={{
+              latitude: 57.709127,
+              longitude: 11.934746,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
+       
+     
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 export default LocationScreen;
