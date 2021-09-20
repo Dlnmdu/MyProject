ade22a0b5ba26a14d58d0ad92064f8e51e719bfc
Dulan <79652708+Dlnmdu@users.noreply.github.com>
2021-09-20 18:00:21 +0530

    Added feature for send current location

--- src/screens/Emergency/EmergencyScreen.js
+++ src/screens/Emergency/EmergencyScreen.js
@@ -1,13 +1,97 @@
-import {Image, Text, TouchableOpacity, View} from 'react-native';
+import {Image, Text, TouchableOpacity, View, Alert} from 'react-native';
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logAddinfoStyle from '../../../styles/logAddinfoStyle';
 import style from './styles';
+import SendSMS from 'react-native-sms';
+import Geolocation from 'react-native-geolocation-service';
 
 const EmergencyScreen = props => {
+  const [mobileNumber, SetMobileNumber] = useState('0767765412');
+
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [bodySMS, setBodySMS] = useState('');
+
+  useEffect(() => {
+    setBodySMS(
+      `https://www.google.com/maps/search/?api=1&query=${latitude}%2C${longitude}`,
+    );
+  }, []);
+
+  const handleLocationPermission = async () => {
+    let permissionCheck = '';
+
+    permissionCheck = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+
+    if (
+      permissionCheck === RESULTS.BLOCKED ||
+      permissionCheck === RESULTS.DENIED
+    ) {
+      const permissionRequest = await request(
+        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+      );
+      permissionRequest === RESULTS.GRANTED
+        ? console.warn('Location permission granted.')
+        : console.warn('location permission denied.');
+    }
+  };
+
+  useEffect(() => {
+    handleLocationPermission();
+  }, []);
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      error => {
+        console.log(error.code, error.message);
+      },
+      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+    );
+  }, []);
+
+  //console.log('print latitude...', latitude);
+  //console.log('print longtiude...', lo);
+
+  const initiateSMS = () => {
+    if (mobileNumber.length != 10) {
+      alert('Please insert correct contact number');
+      return;
+    }
+
+    SendSMS.send(
+      {
+        // Message body
+        body: bodySMS,
+        // Recipients Number
+        recipients: [mobileNumber],
+        // An array of types
+        // "completed" response when using android
+        successTypes: ['sent', 'queued'],
+      },
+
+      (completed, cancelled, error) => {
+        console.log('running........');
+        if (completed) {
+          console.log('SMS Sent Completed');
+          Alert.alert('message Sent');
+        } else if (cancelled) {
+          console.log('SMS Sent Cancelled');
+        } else if (error) {
+          console.log('Some error occured');
+        }
+      },
+    );
+  };
+
   return (
     <View style={style.emergScreenContainer}>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={initiateSMS}>
         <Image
           style={{width: 300, height: 300, top: 150, left: 30}}
           source={require('../../../assets/HelpButton.png')}
--- src/screens/Location/index.js
+++ src/screens/Location/index.js
@@ -1,29 +1,69 @@
-import React from 'react';
-import {View, Text, Image, StyleSheet, SafeAreaView} from 'react-native';
-import style from './styles';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, SafeAreaView, Platform} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {check, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
+import Geolocation from 'react-native-geolocation-service';
 
 const LocationScreen = () => {
+  const [location, setLocation] = useState(null);
+
+  const handleLocationPermission = async () => {
+    let permissionCheck = '';
+
+    permissionCheck = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+
+    if (
+      permissionCheck === RESULTS.BLOCKED ||
+      permissionCheck === RESULTS.DENIED
+    ) {
+      const permissionRequest = await request(
+        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+      );
+      permissionRequest === RESULTS.GRANTED
+        ? console.warn('Location permission granted.')
+        : console.warn('location permission denied.');
+    }
+  };
+
+  useEffect(() => {
+    handleLocationPermission();
+  }, []);
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+        setLocation({latitude, longitude});
+      },
+      error => {
+        console.log(error.code, error.message);
+      },
+      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+    );
+  }, []);
+
+  console.log('print location', location);
+
   return (
     <SafeAreaView style={styles.container}>
-      
-        
-          {/* <Image
-          style={style.imageStyle}
-          source={require('../../../assets/location.jpg')}
-        /> */}
-          <MapView
-            style={styles.map}
-            provider={PROVIDER_GOOGLE}
-            initialRegion={{
-              latitude: 57.709127,
-              longitude: 11.934746,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
-            }}
-          />
-       
-     
+      {location && (
+        <MapView
+          style={styles.map}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.0922,
+           longitudeDelta: 0.0421,
+          }}
+          showsUserLocation={true}
+        />
+        // <MapView.Marker
+        // coordinate={{
+          
+        // }}
+        // />
+      )}
     </SafeAreaView>
   );
 };
@@ -35,6 +75,7 @@ const styles = StyleSheet.create({
   },
   map: {
     ...StyleSheet.absoluteFillObject,
+    flex: 1,
   },
 });
 
